2a63969be6ebf2ecaf9d697ddeb784c321a343a7
Ya'Kuana Davis <ydavis@bowdoin.edu>
2021-01-01 23:35:24 -0600

    Jan1 group session.

--- Individual.java
+++ Individual.java
@@ -1,12 +1,8 @@
 public class Individual {
-<<<<<<< HEAD
-    String sentence;
-    float score; 
-=======
 
     String target;  //just put this here for now, probs doesn't go here
     String indvString; 
-    float fitness; 
+    Integer fitness; 
 
     public Individual(String targetIn, String indv) {
             target = targetIn;
@@ -15,29 +11,31 @@ public class Individual {
     }
     
 
-    //make alpahabet to generate intial random string
-
     //calculates the fitness
-    public float calculateFitness() {
+    public int calculateFitness() {
         fitness = 0;
-        sharedCount = 0;
+        int sharedCount = 0;
 
-        //count number of characters individua; shares with target string, make this the fit
-        for (int i = 0; i < sentence.length(); i++) {
-            thisChar = indvString.charAt(i);  //character in this individuals string at i
-            targetChar = target.chatAt(i);  //character in target string at i
-            if(thisChar == targetChar);  {
-                sharedChar += 1;
+        //count number of characters individual shares with target string, make this the fit
+        for (int i = 0; i < indvString.length(); i++) {
+            char thisChar = indvString.charAt(i);   //character in this individuals string at i
+            char targetChar = target.charAt(i);     //character in target string at i
+            if (thisChar == targetChar);  {
+                sharedCount += 1;
             }
         }
 
-        fitness = sharedChar / indvString.length();  //might have to typecast string length?
+        fitness = sharedCount;  
         return fitness;
     }
 
     //returns fitness if we need it outside of this class
-    public float getFitness() {
+    public int getFitness() {
         return fitness; //or return this.calculateFitness()? idk how to java
     }
->>>>>>> 1a12eed01144040230f36b7cbeafc56d4cc070ac
+
+    public String getIndv() {
+        return indvString;
+    }
 }
+
--- Population.java
+++ Population.java
@@ -1,36 +1,74 @@
 //this class will hold all idnvidiuals in population in an array
 
+import java.util.Random; 
+import java.util.Arrays;
 
 public class Population {
 
     Individual[] individualList;  //list of this population
+    
 
     //Constructor method
-    public Population() {
+    public Population(Individual[] population) {
+        individualList = population;
         sortPop();
     }
-        
 
+    public Population(int size, String target) {
+        generateInitialPop(size, target);
+        sortPop();
+    }
+    
     //generates 100 random individuals for starting population
-    public void generateInitialPop() {
-        //for each individual in poplation (100)
-        //make a new indiivudal with random characters in alphabet
+    public void generateInitialPop(int size, String target) {
+        // for each individual in poplation of size (size)
+        // make a new indiivudal with random characters from ASCII table 
+        final String LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+        final String UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        final String DIGITS = "0123456789";
+        final String SYMBOLS = "!@#$%^&*()_+=-[]{}|;’:\",./<>?";
+        final String SPACE = " ";
+        final String ALL_ASCII = LOWER_CASE + UPPER_CASE + DIGITS + SYMBOLS + SPACE; 
+        final String TARGET = "I think this is a reasonable medium sized string!!";
+        
+        int i, j; 
+        int lenTarget = target.length();
+        Character nextChar; 
+        Individual initialPopulation[] = new Individual[size]; 
+        Random randomGenerator = new Random();  
+
+        for (i = 0; i < size; i++) {
+            String newIndividual = "";
+            
+            for (j = 0; j < lenTarget; j++) {
+                int index = randomGenerator.nextInt(size); 
+                nextChar = ALL_ASCII.charAt(index); 
+                newIndividual += nextChar; 
+            }
+
+            Individual individual = new Individual(target, newIndividual);
+            initialPopulation[i] = individual; 
+        } 
+
+        this.individualList = initialPopulation;
     }
 
-    public void sortPop(){
-        //sort individuals in population based on fitness
+    public Individual[] sortPop() {
+        // sort individuals in population based on fitness
+        Arrays.sort(this.individualList, (ind1, ind2) -> Integer.valueOf(ind1.fitness).compareTo(Integer.valueOf(ind2.fitness)));
+
+        return this.individualList; 
     }
 
-    //should return individual with greatest fitness
-    //problem here is that it wont let me say what is being returned Object or indiivudal?
-    public getFittestIndv {
-        individualList.sortPop();
+    // return individual with greatest fitness
+    public Individual getFittestIndv() {
+        this.sortPop();
         return individualList[0];
     }
     
     //one for adding a new individual(child) to pop
 
-    
+   
 
     
 
